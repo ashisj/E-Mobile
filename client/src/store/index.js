f3af796eea23fdf5fef9081fc0331cf889f20762
Ashis Jena <ashis.jena96@gmail.com>
2019-08-18 20:44:58 +0530

    heroku6

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,13 +3,14 @@ import thunk from 'redux-thunk';
 
 import rootReducer from "./reducers";
 
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
-
+let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && 
+window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+    devTools = a => a;
+}
 const middleware = [thunk];
 
 const defaultState = {}
-
-console.log(devTools);
 
 const store = createStore(
     rootReducer,
